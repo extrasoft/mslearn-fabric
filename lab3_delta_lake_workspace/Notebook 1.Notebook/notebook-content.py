f0655cd099744ed6ede5b63fb46260b1db85cdad
extrasoft <30718043+extrasoft@users.noreply.github.com>
2025-06-24 12:37:21 +0700

    Committing 2 items from workspace 9404bfdd-1add-494d-a549-2ccbe50bb469

---
+++ lab3_delta_lake_workspace/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,357 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f3c41234-b4fe-4906-a05e-f33019f24f82",
+# META       "default_lakehouse_name": "lab3_lakehouse",
+# META       "default_lakehouse_workspace_id": "9404bfdd-1add-494d-a549-2ccbe50bb469",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f3c41234-b4fe-4906-a05e-f33019f24f82"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+#  # Delta Lake tables 
+#  Use this notebook to explore Delta Lake functionality 
+
+# CELL ********************
+
+from pyspark.sql.types import StructType, IntegerType, StringType, DoubleType
+
+# define the schema
+schema = StructType() \
+.add("ProductID", IntegerType(), True) \
+.add("ProductName", StringType(), True) \
+.add("Category", StringType(), True) \
+.add("ListPrice", DoubleType(), True)
+
+df = spark.read.format("csv").option("header","true").schema(schema).load("Files/products/products.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/products/products.csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Create Delta tables
+
+# MARKDOWN ********************
+
+# ###### Create a managed table
+
+# CELL ********************
+
+df.write.format("delta").saveAsTable("managed_products")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ###### Create an external table
+
+# MARKDOWN ********************
+
+# In the Explorer pane, in the … menu for the Files folder, select Copy ABFS path. The ABFS path is the fully qualified path to the lakehouse Files folder.
+
+# CELL ********************
+
+#df.write.format("delta").saveAsTable("external_products", path="abfs_path/external_products")
+df.write.format("delta").saveAsTable("external_products", path="abfss://lab3_delta_lake_workspace@onelake.dfs.fabric.microsoft.com/lab3_lakehouse.Lakehouse/Files/external_products")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Compare managed and external tables
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DESCRIBE FORMATTED managed_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DESCRIBE FORMATTED external_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DROP TABLE managed_products;
+# MAGIC DROP TABLE external_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Use SQL to create a Delta table
+
+# MARKDOWN ********************
+
+# Create an External Table
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC -- เป็น code สำหรับสร้าง External Table
+# MAGIC CREATE TABLE products
+# MAGIC USING DELTA
+# MAGIC LOCATION 'Files/external_products';
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT * FROM products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Explore table versioning
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC UPDATE products
+# MAGIC SET ListPrice = ListPrice * 0.9
+# MAGIC WHERE Category = 'Mountain Bikes';
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DESCRIBE HISTORY products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+delta_table_path = 'Files/external_products'
+# Get the current data
+current_data = spark.read.format("delta").load(delta_table_path)
+display(current_data)
+
+# Get the version 0 data
+original_data = spark.read.format("delta").option("versionAsOf", 0).load(delta_table_path)
+display(original_data)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Analyze Delta table data with SQL queries
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC -- Create a temporary view
+# MAGIC CREATE OR REPLACE TEMPORARY VIEW products_view
+# MAGIC AS
+# MAGIC     SELECT Category, COUNT(*) AS NumProducts, MIN(ListPrice) AS MinPrice, MAX(ListPrice) AS MaxPrice, AVG(ListPrice) AS AvgPrice
+# MAGIC     FROM products
+# MAGIC     GROUP BY Category;
+# MAGIC 
+# MAGIC SELECT *
+# MAGIC FROM products_view
+# MAGIC ORDER BY Category;    
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT Category, NumProducts
+# MAGIC FROM products_view
+# MAGIC ORDER BY NumProducts DESC
+# MAGIC LIMIT 10;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# Alternatively, you can run a SQL query using PySpark
+
+# CELL ********************
+
+from pyspark.sql.functions import col, desc
+
+df_products = spark.sql("SELECT Category, MinPrice, MaxPrice, AvgPrice FROM products_view").orderBy(col("AvgPrice").desc())
+display(df_products.limit(6))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Use Delta tables for streaming data
+
+# MARKDOWN ********************
+
+# Delta Lake supports streaming data. Delta tables can be a sink or a source for data streams created using the Spark Structured Streaming API. In this example, you’ll use a Delta table as a sink for some streaming data in a simulated internet of things (IoT) scenario.
+
+# CELL ********************
+
+from notebookutils import mssparkutils
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# Create a folder
+inputPath = 'Files/data/'
+mssparkutils.fs.mkdirs(inputPath)
+
+# Create a stream that reads data from the folder, using a JSON schema
+jsonSchema = StructType([
+    StructField("device", StringType(), False),
+    StructField("status", StringType(), False)
+])
+iotstream = spark.readStream.schema(jsonSchema).option("maxFilesPerTrigger", 1).json(inputPath)
+
+# Write some event data to the folder
+device_data = '''{"device":"Dev1","status":"ok"}
+{"device":"Dev1","status":"ok"}
+{"device":"Dev1","status":"ok"}
+{"device":"Dev2","status":"error"}
+{"device":"Dev1","status":"ok"}
+{"device":"Dev1","status":"error"}
+{"device":"Dev2","status":"ok"}
+{"device":"Dev2","status":"error"}
+{"device":"Dev1","status":"ok"}'''
+
+mssparkutils.fs.put(inputPath + "data.txt", device_data, True)
+
+print("Source stream created...")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Write the stream to a delta table
+delta_stream_table_path = 'Tables/iotdevicedata'
+checkpointpath = 'Files/delta/checkpoint'
+
+# Write as a manage table
+deltastream = iotstream.writeStream.format("delta").option("checkpointLocation", checkpointpath).start(delta_stream_table_path)
+print("Streaming to delta sink...")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT * FROM IotDeviceData;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+deltastream.stop()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
